80344bd29bab4170c8da3a1fa75e5795496bc1c9
Mrinal Fouzdar <100467560+mrinalFouzdar@users.noreply.github.com>
2023-03-08 23:29:54 +0530

    jwt-verify

--- Backend/passportconfig.js
+++ Backend/passportconfig.js
@@ -3,7 +3,7 @@ const passport = require("passport")
 const JwtStrategy = require("passport-jwt").Strategy;
 const { ExtractJwt } = require("passport-jwt");
 const bcrypt = require("bcryptjs");
-
+const Config = require("../Backend/src/config/config")
 const User = require("./src/Models/registerModel");
 
 
@@ -70,3 +70,49 @@ passport.use(
         }
     )
 );
+
+passport.use(
+    "jwtAdm",
+    new JwtStrategy({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: Config.jwt.secret,
+    },
+    async (payload,done)=>{
+      try {
+          // console.log(payload);
+  
+          if(!payload.sub){
+              return done(null,{id:0})
+          }
+  
+          if(!payload.name){
+              return done(null,{id:0})
+          }
+  
+          if(!payload.exp){
+              return done(null,{id:0})
+          }else{
+              const current_time = Math.round(new Date().getTime() / 1000);
+              if (current_time > payload.exp) {
+                  return done(null, { id: 0 });
+              }
+          }
+  
+          const user = await User.findOne({email:cryptr.decrypt(payload.name)});
+          // console.log("user", user);
+          if(user){
+              // console.log("user[0]",user[0]);
+              done(null, user[0]);
+  
+          }else{
+              return done(null, { id: 0 });
+          }
+  
+      } catch (error) {
+          console.log(error);
+          return done(error,user[0]);
+      }
+    }
+    
+    )
+  );
